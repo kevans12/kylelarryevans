9785c15c0164034db2e309067d44f9c834bd2b88
Kyle Evans <kylelarryevans@gmail.com>
2016-12-25 13:57:38 -0700

    navbar fixes

--- js/jquery.js
+++ js/jquery.js
@@ -37,5 +37,36 @@ $(".navbar-nav li a").click(function(event) {
     if (!$(this).parent().hasClass('dropdown'))
         $(".navbar-collapse").collapse('hide');
 });
+//navbar
+$("#bs-example-navbar-collapse-1 li").on("click", function() {
+    $("li").removeClass("active");
+    $(this).addClass("active");
+  });
+
+function openProdInfo(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+//to get the collapsed dropdown to close on a click
+$(".navbar-nav li a").click(function(event) {
+    if (!$(this).parent().hasClass('dropdown'))
+        $(".navbar-collapse").collapse('hide');
+});
 
 });
